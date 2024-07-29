5832d84ca7360d0f30ea88e37f77891690bd4884
pardeepnitkkr2022 <127744878+pardeepnitkkr2022@users.noreply.github.com>
2024-07-29 17:48:03 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0', // This ensures Vite binds to the correct host
+  },
   plugins: [react()],
 })
